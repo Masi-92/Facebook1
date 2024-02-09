491e0d79d2873d0178fdc6204c855da52b64fd87
Masi-92 <masoud.izadi@92gmali.com>
2024-02-09 10:25:08 +0100

    midelweare

--- FaceBookBack/routers/post.router.js
+++ FaceBookBack/routers/post.router.js
@@ -6,13 +6,14 @@ import {
   getDetails,
   getPost,
 } from "../controllers/post.controller.js";
+import { auth } from "../middleware/authMiddlware.js";
 
 const route = Router();
 
 route.get("/", getPost);
-route.post("/", create);
-route.get("/:id", getDetails);
-route.put("/:id", editLike);
+route.post("/",auth ,create,);
+route.get("/:id",auth, getDetails);
+route.put("/:id",auth, editLike);
 route.delete("/:id",deletePost)
 
 export default route;
